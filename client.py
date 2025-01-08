ab3092d327521fff258466a5f4ecc0f6bee5125c
Jedrzej Kotkowski <jedrzejk143@gmail.com>
2025-01-08 23:44:45 +0100

    _run

--- client.py
+++ client.py
@@ -5,22 +5,38 @@ class HelmClient:
     client_version: str = ""
     client_path: str = "helm"
 
-    def __init__(self, path: str | None) -> None:
+
+    def __init__(self, path: str = ""):
         if path:
             self.client_path = path
 
         version = self.version()
         self.client_version = version
 
+
     def version(self) -> str:
         if self.client_version:
             return self.client_version
 
-        cmd = ["helm", "version", "--short"]
-        o = subprocess.run(cmd, capture_output=True, text=True)
+        stdout, _ = self._run("version", "--short")
 
-        o.check_returncode()
-        return o.stdout.rstrip()
+        return stdout.rstrip()
 
-    def _run(self, args: list[str] | str, *flags: list[str]):
-        pass 
+
+    def _run(self, args: list[str] | str, *flags: str) -> tuple[str, str]:
+        cmd = [self.client_path]
+        
+        if type(args) is list:
+            cmd.extend(args)
+        elif type(args) is str:
+            cmd.append(args)
+        else:
+            raise TypeError
+
+        cmd.extend(flags)
+
+        # TODO: error handling
+        r = subprocess.run(cmd, capture_output=True, text=True)
+
+        return r.stdout, r.stderr
+
